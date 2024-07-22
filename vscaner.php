8ec2b51a5c4a45ecab7e22d5f04be0641abc50e6
bijaya <bijaya@rankwatch.com>
2024-07-22 17:57:28 +0530

    virus update

--- vscaner.php
+++ vscaner.php
@@ -1833,7 +1833,7 @@ BANNER,
 
 #die(print_r(build_blacklisted(true)));
 ###rsync -rapv  root@67.225.161.14:/var/imunify360/files/sigs  ./v1/var/imunify360/files/  
-die(build_db('v1_2024-03-18T213128.982416Z'));
+die(build_db('v1_2024-07-20T062706.327391Z'));
 
 
 function wpscan() {
