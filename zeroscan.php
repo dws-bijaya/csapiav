654f0efb3f44d6d29cf1970b0570ab89aea3e7e4
Bijaya Kumar <bijaya.kumar@digitalwebsolutions.in>
2026-06-02 14:29:49 +0530

    save

--- zeroscan.php
+++ zeroscan.php
@@ -1796,7 +1796,6 @@ register_shutdown_function('__shutdown__');
                     if ( $scanfile[3] !== 'php' )
                         return false;
 
-               # print_r($scanfile);die;
                     global $CONST_CLASS_RESULT;
 
                     $detect = self::detect_hacklink_malware($content);
